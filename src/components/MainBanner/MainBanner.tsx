0c8f1abb70c4ca42f2a8735f65cf3ad5ea9e030a
Daniel <gendik38@gmail.com>
2023-03-28 10:29:12 +0200

    change text-center to text-left

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -6,7 +6,7 @@ const MainBanner: React.FC = () => {
     <div className="main-banner h-[500px]lg:h-[600px] w-screen">
       <div className="grid-net grid grid-cols-1 lg:grid-cols-5 grid-rows-1">
         <div className="grid-item-1 h-[250px] lg:h-[600px] bg-main-dark-blue col-span-2 flex flex-col items-center justify-center gap-10 ">
-            <h1 className="uppercase text-white text-center leading-10 font-bold text-4xl lg:text-6xl">disover your<br/> dream house</h1>
+            <h1 className="uppercase text-white text-left leading-10 font-bold text-4xl lg:text-6xl tracking-wider">discover your<br/> dream house</h1>
             <p className="text-white lg:text-xl">Buying a home is a life-changing experience, so shouldn't  <br/> 
             your real estate agent be a life changer</p>
         </div>
