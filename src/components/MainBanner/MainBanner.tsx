73d5aae8c6a2b132fbd7e684b56d18b66ad298c5
Daniel <gendik38@gmail.com>
2023-03-28 16:22:08 +0200

    making everything responsive

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -1,18 +1,20 @@
 import React from "react"
 import House_image from "../../assets/Main_house_image.png"
+import SearchBox from "./SearchBox"
 
 const MainBanner: React.FC = () => {
   return (
-    <div className="main-banner h-[500px]lg:h-[600px] w-screen">
+    <div className="main-banner h-[500px] md:h-[700px] lg:h-[600px] w-screen">
       <div className="grid-net grid grid-cols-1 lg:grid-cols-5 grid-rows-1">
-        <div className="grid-item-1 h-[250px] lg:h-[600px] bg-main-dark-blue col-span-2 flex flex-col items-center justify-center gap-10 ">
+        <div className="grid-item-1 h-[250px] md:h-[350px] lg:h-[600px] bg-main-dark-blue col-span-2 flex flex-col items-center justify-center gap-5 z-20">
             <h1 className="uppercase text-white text-left leading-10 font-bold text-4xl lg:text-6xl tracking-wider">discover your<br/> dream house</h1>
-            <p className="text-white lg:text-xl">Buying a home is a life-changing experience, so shouldn't  <br/> 
+            <p className="text-white lg:text-xl mb-10">Buying a home is a life-changing experience, so shouldn't  <br/> 
             your real estate agent be a life changer</p>
+            <SearchBox />
         </div>
 
         <div
-          className="grid-item-2 h-[250px] lg:h-[600px] bg-no-repeat col-span-3 bg-cover"
+          className="grid-item-2 h-[250px] md:h-[350px] lg:h-[600px] bg-no-repeat col-span-3 bg-cover z-10"
           style={{ backgroundImage: `url(${House_image})` }}
         ></div>
       </div>
---
+++ src/components/MainBanner/SearchBox.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const SearchBox:React.FC = () => {
+  return (
+    <>
+    <div className="search-box lg:w-[750px] bg-white rounded-md lg:-mr-96 xl:-mr-56 -mb-72 lg:-mb-0 z-50 py-6">
+        <div className="box-content grid grid-cols-2 lg:grid-cols-4 mx-3 gap-5">
+            <div className="grid-item-1 flex justify-between flex-col">
+                <h4 className="text-main-light-blue font-bold">Location</h4>
+                <input type="text" defaultValue={"Hamburg,Germany"} className="bg-[#E2E2E2] rounded-md text-center py-2" />
+                    </div>
+
+            <div className="grid-item-2 flex justify-between flex-col">
+                <h4 className="text-main-light-blue font-bold">Type</h4>
+                <input type="text" defaultValue={"Apartment"} className="bg-[#E2E2E2] rounded-md text-center py-2" />
+                    </div>
+
+                    <div className="grid-item-2 flex justify-between flex-col">
+                <h4 className="text-main-light-blue font-bold">Price Range</h4>
+                <input type="text" defaultValue={"900€ - 1200€"} className="bg-[#E2E2E2] rounded-md text-center py-2" />
+                    </div>
+
+                    <div className="grid-item-2 text-center flex items-center justify-center">
+                    <button className="bg-[#4BFF73] text-main-dark-blue font-bold  py-3 rounded-md px-5 hover:opacity-80">Explore<br/> now</button>
+                    </div>
+        </div>
+    </div>
+    </>
+  )
+};
+
+export default SearchBox;
